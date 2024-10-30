ca2b5bb65cd88f012f859c391bfa06bb311cefeb
Luna-bf <lburattifayolle@gmail.com>
2024-10-30 14:37:00 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,33 +2,68 @@
 const elements = {
     allTasks: document.querySelector('#all-tasks ul'),
     form: document.querySelector('#task-form form'),
-    deleteBtn: document.querySelector('#delete-task-btn')
+    deleteBtn: document.querySelector('#delete-task-btn'),
+    deleteAllBtn: document.querySelector('#delete-all-btn')
 };
 
 //J'indique mes priorités et leur valeur
-const PRIORITY_HIGH = 1;
-const PRIORITY_NORMAL = 2;
-const PRIORITY_LOW = 3;
+const priorityHigh = 1;
+const priorityNormal = 2;
+const priorityLow = 3;
 
 let myTasks = [
 {
     title: 'Finir ma todo list',
-    priority: PRIORITY_HIGH,
-    isFinished: false
+    priority: priorityHigh,
+    isChecked: false
 },
 
 {
     title: 'Préparer les cadeaux de Noël',
-    priority: PRIORITY_NORMAL,
-    isFinished: false
+    priority: priorityNormal,
+    isChecked: false
 },
 
 {
     title: 'Finir Hollow Knight à 112%',
-    priority: PRIORITY_LOW,
-    isFinished: false
+    priority: priorityLow,
+    isChecked: false
 }
 
 ];
 
 console.log(myTasks);
+
+//La fonction qui va me permettre d'ajouter des tâches
+function displayTasks() {
+    const newTask = document.createElement('label');
+    newTask = document.createTextNode('input');
+
+    
+}
+
+console.log(displayTasks());
+        //<label><input type="checkbox">Task</label>
+
+elements.allTasks.addEventListener('click', (e) => {
+    e.preventDefault();
+});
+
+
+//La fonction qui va me permettre de supprimer uniquement les tâches terminées
+elements.deleteBtn.addEventListener('click', (e) => {
+    
+    //J'empêche le navigateur de se recharger
+    e.preventDefault();
+    
+});
+
+
+//La fonction qui va me permettre de supprimer toutes les tâches
+elements.deleteAllBtn.addEventListener('click', (e) => {
+    
+    //J'empêche le navigateur de se recharger
+    e.preventDefault();
+    
+    myTasks.pop();
+});
