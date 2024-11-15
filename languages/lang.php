f5e45189e377c323b9e09582661f5aee6bc09bb1
Luna-bf <lburattifayolle@gmail.com>
2024-11-15 10:37:40 +0100

    Update lang.php

--- languages/lang.php
+++ languages/lang.php
@@ -7,4 +7,18 @@ if(session_statut() === PHP_SESSION_NONE) {
   session_start(); #Si aucune session n'est ouverte, on en ouvre une nouvelle et on exécute le reste du code.
 }
 
+require 'getLanguages';
+
+function getLanguages() {
+  
+  #Je vérifie si il existe une autre langue (?? / équivalent de isset), si ce n'est pas le cas, je met le français comme langue par défaut.
+  $_SESSION['lang'] = $_SESSION['lang'] ?? 'fr';
+
+  #Si l'utilisateur change la langue alors il faut récupérer la langue en question (GET) si l'utilisateur ne change pas la langue alors on remet la langue par défaut.
+  $_SESSION['lang'] = $_GET['lang'] ?? $_SESSION['lang'];
+
+  #Enfin, je concatène $_SESSION['lang'] à l'extension .php pour aller chercher le fichier correspondant à la langue sélectionnée. Cela fonctionne même si je rajoute 50 langues.
+  return 'languages/'.$_SESSION['lang'].'.php';
+}
+
 ?>
