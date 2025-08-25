a997c87cd8a6a44e8dc950f362a3e6df6bad5207
Luna-bf <lburattifayolle@gmail.com>
2025-08-25 14:19:40 +0200

    Added task deletion

--- js/main.js
+++ js/main.js
@@ -1,33 +1,58 @@
 const elements = {
     addTaskNameInput: document.getElementById("task-name"),
-    allTasksUl: document.querySelector("#all-tasks ul"),
-    form: document.getElementById('task-form'),
+    allTasksOl: document.querySelector("#all-tasks ol"),
+    form: document.getElementById("task-form"),
     addTaskButton: document.getElementById("add-task-btn"),
+    deleteAllTasksButton: document.getElementById("delete-all-tasks-btn"),
 }
 
+
 let tasks = [];
+
 
 document.addEventListener('DOMContentLoaded', function () {
 
     console.log('DOM loaded');
     createNewTask();
-})
+
+    deleteAllTasks();
+});
+
 
 function createNewTask() {
 
-    elements.allTasksUl.innerHTML = '';
+    elements.allTasksOl.innerHTML = '';
 
     // Pour chaque task du tableau tasks:
     tasks.forEach(task => {
 
-        // Je créé un élément "li" et "input" puis j'assigne un type 'checkbox' à mon élément input
+        // Je créé un élément "li" et "label"
         const li = document.createElement('li');
-        // const input = document.createElement('input');
-        // const label = document.createElement('label');
-        // input.type = "checkbox";
+        const label = document.createElement('label');
 
-        li.textContent += task.title; //J'accède aux propriétés de l'objet newTask pour obtenir le texte de la tâche
-        elements.allTasksUl.append(li);
+        const inputCheckbox = document.createElement('input'); // Je créé aussi un élément inputCheckbox
+        inputCheckbox.type = "checkbox"; // J'assigne un type à l'élément inputCheckbox
+
+        label.textContent += task.title; // J'accède aux propriétés de l'objet newTask pour obtenir le texte de la tâche (title) et je l'ajoute à l'élément label avec la propriété textContent
+
+        label.prepend(inputCheckbox); // J'ajoute l'élément input au début du label (avant task.title)
+        li.append(label); // Puis j'ajoute l'élément label au li 
+
+        elements.allTasksOl.append(li); // Enfin, j'ajoute l'élément li à la liste ol
+
+
+        // Evènement pour gérer l'état d'une tâche (complétée ou non) avec la propriété "change"
+        inputCheckbox.addEventListener("change", function () {
+
+            if (inputCheckbox.checked) {
+                task.isDone = true;
+                label.style.textDecoration = "line-through";
+
+            } else {
+                task.isDone = false;
+                label.style.textDecoration = "none";
+            }
+        });
     });
 }
 
@@ -42,6 +67,7 @@ elements.form.addEventListener('submit', function (e) {
     d'ajouter de nouvelles fonctionnalités (id, priorité, catégorie...) */
     const newTask = {
         title: formData.get('task_name'), //Je récupère la donnée de l'input nommé "task_name" (name="task_name")
+        isDone: false, //Par défaut, la tâche n'est pas terminée
     };
 
     // J'ajoute cette nouvelle tâche (objet) au tableau tasks
@@ -49,4 +75,18 @@ elements.form.addEventListener('submit', function (e) {
 
     //Puis je met le contenu à jour en appelant la fonction createNewTask
     createNewTask();
+    elements.form.reset();
 });
+
+
+function deleteAllTasks() {
+
+    elements.deleteAllTasksButton.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        tasks.length = 0; // Supprime les éléments du tableau
+        elements.allTasksOl.innerHTML = ''; // Supprime les éléments de la page HTML
+
+        console.log(tasks);
+    })
+};
