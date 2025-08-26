cb6adc3f91f9c045c46c82ee4762ae2734525282
Luna-bf <lburattifayolle@gmail.com>
2025-08-26 09:38:51 +0200

    added correction

---
+++ js/correction.js
@@ -0,0 +1,10 @@
+const elements = {
+    addTaskNameInput: document.getElementById("task-name"),
+    allTasksOl: document.querySelector("#all-tasks ol"),
+    form: document.getElementById("task-form"),
+    addTaskButton: document.getElementById("add-task-btn"),
+    deleteAllTasksButton: document.getElementById("delete-all-tasks-btn"),
+}
+
+let tasks = [];
+
