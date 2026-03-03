9d44eb9702150b1a7ae801ed49aa1049e2029586
Luna-bf <lburattifayolle@gmail.com>
2026-03-03 16:25:17 +0100

    Construction de la logique : j'ai ajouté la récupération (findTask, findAllTasks, getCategories et getPriorities), la logique de création est en cours de construction

--- src/model/Database.php
+++ src/model/Database.php
@@ -8,7 +8,10 @@ Note : le namespace indique où se situe la classe dans la structure des dossier
 j'ai besoin d'accéder à la classe : ici j'indique que la classe se trouve dans le dossier "model", un sous-dossier du dossier
 "src". Ajouter "Database" signifierait que "Database" est un sous-dossier de "model", ce qui n'est pas le cas ici.
 */
+
 namespace src\model;
+
+use Exception;
 use PDO; // Importation de la classe "PDO", qui va me permettre de communiquer avec la BDD
 
 // Cette classe va superviser toutes les opérations liées à la base de données
@@ -38,10 +41,9 @@ class Database
      * @param string $table : Le nom de la table
      * @return array : Cette méthode retourne un tableau
      */
-    public function findAll($table)
+    public function findAllTasks($table)
     {
-        // Revoir à quoi sert cette requête et pourquoi je la déclare comme ça
-        return $this->pdo->query("SELECT * FROM $table")->fetchAll(PDO::FETCH_ASSOC);
+        return $this->pdo->query("SELECT t.*, c.category_name, p.priority_id FROM $table as t JOIN categories as c on t.category_id = c.category_id JOIN priorities as p on t.priority_id = p.priority_id ORDER BY p.priority_id ASC");
     }
 
     /**
@@ -51,15 +53,15 @@ class Database
      * @param string $table : Le nom de la table
      * @return : Cette méthode retourne un tableau associatif si la donnée existe, ou false si elle n'existe pas
      */
-    public function find($id, $table)
+    public function findTask($id, $table)
     {
         /*
         - Je prépare une requête qui va récupérer une seule ligne de la table grâce à un identifiant donné,
         - J'exécute la requête en passant l'identifiant récupéré en tant que paramètre dans la requête,
         - Puis je renvoie la ligne récupérée grâce à la méthode "fetch()" de PDO.
         
-        FETCH_ASSOC signifie que le résultat sera renvoyé sous forme de tableau associatif, soit :
-        $ligne = [
+        Note : FETCH_ASSOC signifie que le résultat sera renvoyé sous forme de tableau associatif, soit :
+        $task = [
             "task_id" => 12,
             "category_id" => 3,
             "priority_id" => 2,
@@ -67,16 +69,45 @@ class Database
             "is_complete" => 0,
         ];
         */
-        // $this fait référence à l'objet actuel (ici, Database)
-        $statement = $this->pdo->prepare("SELECT * FROM $table WHERE id = :id");
+        $statement = $this->pdo->prepare("SELECT * FROM $table WHERE id = :id"); // $this fait référence à l'objet actuel (ici, Database)
         $statement->execute(["id" => $id]);
         return $statement->fetch(PDO::FETCH_ASSOC);
     }
 
+    public function getCategories()
+    {
+        // Revoir à quoi sert cette requête et pourquoi je la déclare comme ça
+        return $this->pdo->query("SELECT * FROM categories ORDER BY category_id ASC");
+    }
+
+    public function getPriorities()
+    {
+        // Revoir à quoi sert cette requête et pourquoi je la déclare comme ça
+        return $this->pdo->query("SELECT * FROM priorities ORDER BY priority_id ASC");
+    }
+
     // Les autres méthodes qui vont me permettre de manipuler les données de la BDD
-    public function save($data, $table) {}
+    public function createTask($task)
+    {
+        if (isset($_POST['add'])) {
 
-    public function update($id, $data, $table) {}
+            if (!empty(!empty($_POST['category']) && !empty($_POST['priority']) && $_POST['task_name'])) {
 
-    public function delete($id, $data, $table) {}
+                $category = $_POST['category'];
+                $priority = $_POST['priority'];
+                $task_name = $_POST['name'];
+
+                $task = $this->pdo->prepare("INSERT INTO tasks(category_id, priority_id, task_name) VALUES(:category_id, :priority_id, :task_name)");
+                $task->execute(['category_id' => $category, 'priority_id' => $priority, 'task_name' => $task_name]);
+
+                if (!$task) {
+                    echo "Une erreur est survenue : l'ajout de la tâche n'a pas pu être effectuée.";
+                }
+            }
+        }
+    }
+
+    public function updateTask($id, $data, $table) {}
+
+    public function deleteTask($id, $data, $table) {}
 }
