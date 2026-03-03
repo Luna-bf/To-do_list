b29199e8434bbe54014616209c4f733075245a52
Luna-bf <lburattifayolle@gmail.com>
2026-03-03 14:17:33 +0100

    Mise en place de l'architecture MVC

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// J'importe les contrôleurs à l'aide de leur namespace
+use src\controllers\HomeController;
+
+// Je défini le chemin d'accès de chaque dossier ici :
+define('ROOT', dirname(__DIR__)); // dirname(__DIR__) va récupérer le nom du dossier parent (ici "Trouver-Ma-Traduction") et le stocker dans la constante nommée "ROOT"
+define('TEMPLATES', ROOT . '/templates'); //Je déclare une constante nommée "VIEWS" qui prend comme valeur la racine du projet concaténé avec la chaine de caractères "/views", ce qui donne le chemin complet suivant : racine_du_projet/views
+
+require_once ROOT . "/vendor/autoload.php"; // Va charger toutes les librairies présentes dans le dossier vendors ainsi que les classes du dossier src. Cela remplace la fonction spl_autoload que j'avais déclaré avant (voir fichier spl-autoload.php dans le dossier src)
+
+/* PATH_INFO correspond à (à développer) */
+switch ($_SERVER['PATH_INFO'] ?? '/') { // J'utilise un switch pour gérer les différents appels de fichiers
+
+    // HomeController
+    case '/': // Ce chemin défini index.html.twig comme page d'accueil, j'arriverais sur cette page lorsque je lancerais le serveur
+        (new HomeController())->index(); // Je créé un objet HomeController() qui appelle la méthode index() afin d'appeler le fichier index.html.twig
+        break;
+
+    default:
+        echo "Page introuvable.";
+        break;
+}
---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace src\controllers;
+
+// La classe HomeController hérite de la classe BaseController : elle récupère ses propriétés et méthodes
+class HomeController extends BaseController {
+
+    // La méthode render() permet d'appeler un fichier spécifique grâce à son chemin d'accès et de lui transmettre des données (voir BaseController.php)
+    public function index() {
+        /*
+        J'assigne le chemin d'accès au fichier à la variable $path et lui transmet des données grâce à la variable $data : $path
+        renvoie le fichier "index.html.twig" et $data renvoie une variable $sentence qui contient une chaîne de caractères que je
+        vais pouvoir afficher dans la page.
+        */
+        $this->render('home/index.html.twig', ['sentence' => 'Hello World !']);
+    }
+}
+
+// php -S localhost:8000
