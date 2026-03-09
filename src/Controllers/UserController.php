749190d64208544ddaaff530e354a52db2aeb86b
Luna-bf <lburattifayolle@gmail.com>
2026-03-09 15:06:45 +0100

    Ajout de la logique d'inscription de l'utilisateur dans le fichier UserController

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -9,20 +9,37 @@ use Exception;
 class UserController extends BaseController {
 
     public function register() {
-        // Je récupère les catégories et les priorités pour les afficher en tant qu'option dans le formulaire
-        // $categories = $this->db->getCategories('categories');
-        // $priorities = $this->db->getPriorities('priorities');
+        if (isset($_POST['register'])) { // Lorsque l'élément nommé "inscription" est cliqué...
 
-        // Je récupère les valeurs saisies dans le formulaire
-        // $task = $this->db->createTask('category', 'priority', 'task_name');
+            // Je récupère les valeurs des différents champs de saisie du formulaire
+            $email = htmlspecialchars(trim($_POST['email'])); // Champ de saisie nommé "email"
+            $username = htmlspecialchars(trim($_POST['username'])); // Champ de saisie nommé "username"
+            $password = htmlspecialchars(trim($_POST['password'])); // etc...
 
-        // if ($task) {
-            // header('Location: /'); // Je redirige vers la page d'accueil (la racine, soit : '/')
-            // exit;
-        // }
+            /*
+            Je vérifie que tous les champs du formulaire ne sont pas vides en utilisant la méthode empty(), utilisée pour
+            vérifier si une variable est vide ou égale à FALSE :
+            */
+            if (!empty($email) && !empty($username) && !empty($password)) { // Si tous les champs du formulaire ne sont pas vide, alors...
 
-        // Je passe les catégories, les priorités et la tâche que je créé en tant que valeurs à la page
-        $this->render('form/register.html.twig', ["str" => "Bonjour le monde !"]);
+                // Je hache (hash) le mot de passe
+                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+                // Puis je créé l'utilisateur
+                $user = $this->db->createUser($email, $username, $hashed_password);
+
+                if (!$user) { // Si le contenu de la variable $resultat retourne false, alors...
+                    $message = "Une erreur est survenue, l'inscription n'a pas pu être effectuée.";
+                } else {
+                    header('Location: /form/login');
+                    exit;
+                }
+            } else {
+                $message = "Tous les champs de saisie doivent être remplis.";
+            }
+        }
+
+        $this->render('form/register.html.twig', ['email' => $email, 'username' => $username, 'password' => $hashed_password, 'message' => $message]);
     }
 
     public function login() {
