980b4f39f649661d81d4602343c6ddaf193b59d2
Luna-bf <lburattifayolle@gmail.com>
2026-03-12 13:00:19 +0100

    Màj de la page d'accueil : l'utilisateur est maintenant redirigé vers le formulaire d'inscription et ne peut accéder à la todo list qu'après s'être inscrit

--- public/index.php
+++ public/index.php
@@ -10,24 +10,26 @@ define('TEMPLATES', ROOT . '/templates'); //Je déclare une constante nommée "V
 
 require_once ROOT . "/vendor/autoload.php"; // Va charger toutes les librairies présentes dans le dossier vendors ainsi que les classes du dossier src. Cela remplace la fonction spl_autoload que j'avais déclaré avant (voir fichier spl-autoload.php dans le dossier src)
 
-/* PATH_INFO correspond à (à développer) */
+/* PATH_INFO contient le chemin de l'URL, par exemple : si l'URL appelé est "/home" alors le chemin de l'URL sera
+"http://localhost/home". J'utilise l'opérateur de fusion (coalescing operator) afin de vérifier si PATH_INFO existe, si
+c'est le cas, je renvoie vers la racine (la page d'accueil) */
 switch ($_SERVER['PATH_INFO'] ?? '/') { // J'utilise un switch pour gérer les différents appels de fichiers
 
-    // TaskController
-    case '/': // Ce chemin défini index.html.twig comme page d'accueil, j'arriverais sur cette page lorsque je lancerais le serveur
-        (new TaskController())->index(); // Je créé un objet TaskController() qui appelle la méthode index() afin d'appeler le fichier index.html.twig
+    // UserController
+    case '/': // Ce chemin défini register.html.twig comme page d'accueil, j'arriverais sur cette page lorsque je lancerais le serveur
+        (new UserController())->register(); // Je crée un objet UserController() qui appelle la méthode register() afin charger la vue et les données de cette méthode (ici le fichier register.html.twig)
         break;
 
-    case '/form/register':
-        (new UserController())->register();
-        break;
-
-    case '/form/login':
-        (new UserController())->login();
+    case '/form/login': // Si le chemin d'URL de PATH_INFO est égal à "http://localhost/form/login"
+        (new UserController())->login(); // Alors je crée un objet UserController() qui appelle la méthode login() afin charger la vue et les données de cette méthode (ici le fichier login.html.twig)
         break;
 
     case '/logout':
         (new UserController())->logout();
+        break;
+
+    case '/home':
+        (new TaskController())->index();
         break;
 
     case '/task/createTask':
